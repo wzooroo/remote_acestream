aa0750664a6d2ce0ea2476edf7019696135de133
znachok88 <45965783+znachok88@users.noreply.github.com>
2019-01-03 17:54:51 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#скачиваем архив
+mkdir opt1
+wget https://mega.nz/#!8cdhCQAY!RGSOypNzYWWeWqMK8-UxtgxZuqi3LPshoYfJOTgulJw /opt1
+
+
+#розпаковка архива
+
+#unzip /opt1/linux-arm_all.zip 
+
+#remotefork утановка
+
+#cp /opt/remotefork/remotefork.service /etc/#systemd/system
+#chmod +x /opt/remotefork/RemoteForkCP
+#systemctl enable remotefork.service
+
+
+#acestream установка
+
+#cp /opt/acestream/acestream.service /etc/#systemd/system
+#chmod +x /opt/acestream/acestream.start
+#chmod +x /opt/acestream/acestream.stop
+#systemctl enable acestream.service
+#systemctl start acestream.service
+
+#reboot
+#reboot
